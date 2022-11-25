d58ca9e1ef9791dd1fd026302e716691029a3f60
jjjadey <nattakan.premp@gmail.com>
2022-11-26 00:05:59 +0700

    Header(top) component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-       <p className="text-teal-500">Test tailwind...</p>
+       <Header/>
     </div>
    
   )
