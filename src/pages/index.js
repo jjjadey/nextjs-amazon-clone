007eeba3263c0a45857b9a68e72fe0c61c7e14bb
jjjadey <nattakan.premp@gmail.com>
2022-11-26 14:22:27 +0700

    Banner component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-       <Header/>
+    <div className="bg-gray-100 h-screen">
+      <Head>
+        <title>Amazon Clone</title>
+      </Head>
+
+      <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+      </main>
     </div>
-   
+
   )
 }
