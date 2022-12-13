babb95ad0e3bebb352771b1393ae18a9a0bcf2d2
jjjadey <nattakan.premp@gmail.com>
2022-12-13 19:31:53 +0700

    Checkout Product component

---
+++ src/components/CheckoutProduct.js
@@ -0,0 +1,43 @@
+import { StarIcon } from '@heroicons/react/24/solid';
+import Image from 'next/image';
+import Currency from "react-currency-formatter";
+
+const CheckoutProduct = ({ product }) => {
+    const { id, title, price, description, category, image, rating, hasPrime } = product;
+
+    return (
+        <div className="grid grid-cols-5">
+            <Image src={image} height={200} width={200} alt={description} />
+
+            <div className="col-span-3 mx-5">
+                <p>{title}</p>
+                <div className="flex">
+                    {Array(rating).fill().map((_, i) => (
+                        <StarIcon key={i} className="h-5 text-yellow-500" />
+                    ))}
+                </div>
+                <p className="text-xs my-2 line-clamp-3">{description}</p>
+                <Currency quantity={price} currency="GBP" />
+                {hasPrime && (
+                    <div className="flex items-center space-x-2">
+                        <img
+                            className="w-12"
+                            src="https://links.papareact.com/fdw"
+                            alt=""
+                            loading="lazy" />
+                        <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+                    </div>
+
+                )}
+            </div>
+
+            <div className="flex flex-col space-y-2 my-auto justify-self-end">
+                <button className="button mt-auto">Add to Basket</button>
+                <button className="button mt-auto">Remove from Basket</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default CheckoutProduct
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Image from 'next/image';
+import { useSelector } from 'react-redux';
+import { selectItems } from '../slices/basketSlice';
+import CheckoutProduct from '../components/CheckoutProduct';
 
 const checkout = () => {
+
+    const items = useSelector(selectItems);
+
     return (
         <div className="bg-gray-100">
             <Header />
@@ -13,10 +19,16 @@ const checkout = () => {
                     <Image src="https:links.papareact.com/ikj"
                         width={1020}
                         height={250}
+                        alt="banner"
                     />
 
                     <div className="flex flex-col p-5 space-y-10 bg-white">
-                        <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+                        <h1 className="text-3xl border-b pb-4">
+                            {items.length === 0 ? "Your Amazon Basket is empty." : "Shopping Basket"}
+                        </h1>
+                        {items.map((item,i)=>(
+                            <CheckoutProduct key={item.id} product={item}/>
+                        ))}
                     </div>
                 </div>
 
